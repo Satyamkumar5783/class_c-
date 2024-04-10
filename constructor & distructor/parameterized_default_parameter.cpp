10b4e25ab200444d8659e67a6033d1c7f509c8d7
Satyamkumar5783 <satyamK5783@gmail.com>
2024-04-10 12:22:04 +0530

    add default parameters of constructor

--- constructor & distructor/parameterized_default_parameter.cpp
+++ constructor & distructor/parameterized_default_parameter.cpp
@@ -3,12 +3,45 @@ using namespace std;
 
 class student
 {
-    int roll_no,month ;
+    int roll_no,n=600,fee;
     char name [50];
 
 
     public:
-    student(int roll);        //declaration of con
+    student(int month);        //declaration of constructor
     void display();
 
+};
+
+
+student::student(int month=1)  //parameterized constructor 
+{
+    cout<<"\nenter roll no. of student :";
+    cin>>roll_no;
+    cout<<"\nenter the name of student: ";
+    cin>>name;
+
+    fee=month*n;
 }
+
+void student::display()  // function for display
+{
+    cout<<"\nname "<<name<<"\nroll no "<<roll_no<<"\n your total fee "<<fee;
+}
+
+int main()    //main function
+{
+    //object declaration 
+    //student s1,s2;        //here the constructor call itself when we create object
+
+    //we create object with parameters
+    student s1(4),s2(6);
+
+
+    s1.display();
+    s2.display();         //function calling 
+
+    return 0;
+
+}
+
